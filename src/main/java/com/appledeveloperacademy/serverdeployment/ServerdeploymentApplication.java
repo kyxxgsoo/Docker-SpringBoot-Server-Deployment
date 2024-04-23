586fe55f1adcbf85b25ac096ceef13267a115dbf
KyungsooLee <jmtkd9196@gmail.com>
2024-04-23 12:17:37 +0900

    Init Spring Boot Project

---
+++ src/main/java/com/appledeveloperacademy/serverdeployment/ServerdeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.appledeveloperacademy.serverdeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerdeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerdeploymentApplication.class, args);
+	}
+
+}
